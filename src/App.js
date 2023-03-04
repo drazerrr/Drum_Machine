917587e06d7127c5ee7a9c0d6e42ec65141fd544
drazerrr <anuragdwivedi9794@gmail.com>
2023-03-04 19:24:50 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {bankOne, bankTwo} from './source.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: bankOne,
+      display: '',
+      volume: 0.5,
+      switch: false
+    }
+    this.playSound = this.playSound.bind(this);
+    this.volumer = this.volumer.bind(this);
+    this.bankChange = this.bankChange.bind(this);
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', (event) => {
+      this.playSound(event.key.toUpperCase())
+    })
+  }
+  componentWillUnmount() {
 
+    document.removeEventListener('keydown', (event) => {
+      this.playSound(event.key.toUpperCase())
+    })
+  }
+  volumer(e) {
+    this.setState({
+      volume: e.target.value
+
+    })
+  }
+  playSound(e) {
+    let audio = document.getElementById(e);
+    audio.currentTime = 0;
+    audio.volume = this.state.volume;
+    audio.play();
+    this.setState({
+      display: e.replace("-", " ")
+    })
+  }
+  bankChange() {
+   if(this.state.switch){
+    this.setState({
+      data: bankTwo,
+      switch: false,
+      display: "Smooth Piano Kit"
+    })
+   }else {
+    this.setState({
+      data: bankOne,
+      switch: true,
+      display: "Heater Kit"
+    })
+   }
+  }
+    render(){
+      return (
+        <div id="drum-machine">
+         {this.state.data.map((item, index, array) => <button onClick={() => this.playSound(array[index].id)} key={array[index].keyCode} className="drum-pad" id={array[index].keyString}><audio className="clip" id={array[index].id} src={array[index].url}></audio>{array[index].keyString}</button>)
+         }
+         
+         <input type="range" min="0" max="1" step="0.01" value={this.state.volume} onChange={this.volumer} id="volume"></input>
+         <label>Volume: {Math.floor(this.state.volume * 100)}</label>
+         <div id="display">{this.state.display}</div>
+         <button value={this.state.switch} onClick={this.bankChange}>Bank</button>
+        </div>
+      )
+    }
+  }
 export default App;
---
+++ src/source.js
@@ -0,0 +1,93 @@
+    export const bankOne = [{
+    keyCode: 81, // 
+    keyString: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+    keyCode: 87,
+    keyString: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+    keyCode: 69,
+    keyString: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+    keyCode: 65,
+    keyString: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+    keyCode: 83,
+    keyString: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+    keyCode: 68,
+    keyString: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+    keyCode: 90,
+    keyString: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+    keyCode: 88,
+    keyString: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+    keyCode: 67,
+    keyString: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},
+];
+export const bankTwo = [{
+    keyCode: 81,
+    keyString: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+    keyCode: 87,
+    keyString: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+    keyCode: 69,
+    keyString: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+    keyCode: 65,
+    keyString: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+}, {
+    keyCode: 83,
+    keyString: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+    keyCode: 68,
+    keyString: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+    keyCode: 90,
+    keyString: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+    keyCode: 88,
+    keyString: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+}, {
+    keyCode: 67,
+    keyString: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}];
